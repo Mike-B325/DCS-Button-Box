90696385a4317e6513ac5007bd4ca493b6280df8
Michael Beath <michael.charles.beath+GitHub@gmail.com>
2025-08-06 13:26:28 -0700

    Added Arduino Code, DCSBios Library, & Graphing Code

---
+++ Arduino Code/DCSBox_V1/DCSBox_V1.ino
@@ -0,0 +1,153 @@
+/*
+  Tell DCS-BIOS to use a serial connection and use the default Arduino Serial
+  library. This will work on the vast majority of Arduino-compatible boards,
+  but you can get corrupted data if you have too many or too slow outputs
+  (e.g. when you have multiple character displays), because the receive
+  buffer can fill up if the sketch spends too much time updating them.
+  
+  If you can, use the IRQ Serial connection instead.
+*/
+#define DCSBIOS_IRQ_SERIAL
+
+#include "DcsBios.h"
+#include <Wire.h>
+#include <Adafruit_SSD1306.h>
+
+// Definitions
+#define SCREEN_WIDTH 128  // OLED display width, in pixels
+#define SCREEN_HEIGHT 50  // OLED display height, in pixels
+#define OLED_RESET -1      // Reset pin # (or -1 if sharing Arduino reset pin)
+
+// Initialise variables
+int cw = SSD1306_WHITE;
+int cb = SSD1306_BLACK;
+// create what ever variables you need
+double volts;
+double bvolts;
+double x, y;
+
+// these are a required variables for the graphing functions
+bool Redraw1 = true;
+bool Redraw2 = true;
+bool Redraw3 = true;
+bool Redraw4 = true;
+double ox , oy ;
+
+// Start Class
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+int PINFIRE = 5;
+
+bool redrawDial = false;
+
+/* paste code snippets from the reference documentation here */
+DcsBios::ActionButton fireTestBtnToggle("FIRE_TEST_BTN", "TOGGLE", PINFIRE);
+DcsBios::Potentiometer brtConsole("BRT_CONSOLE", A7);
+
+void onSideslipChange(unsigned int newValue) {
+  static unsigned int lastValue = 0;
+  DrawDial(display, newValue, 64, 32, 30, 0, 65535, 10000, 0, 180, "Test", redrawDial);
+}
+DcsBios::IntegerBuffer sideslipBuffer(UH_1H_SIDESLIP, onSideslipChange);
+
+void setup() {
+  //Serial.begin(9600);  // Optional, helpful for debug
+
+  DcsBios::setup();
+
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+   // Serial.println(F("SSD1306 allocation failed"));
+    while (1);
+  }
+
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 0);
+  display.display();
+}
+
+void loop() {
+  DcsBios::loop();
+}
+
+
+void DrawDial(Adafruit_SSD1306 &d, double curval, int cx, int cy, int r, double loval , double hival , double inc, double dig, double sa, String label, bool &Redraw) {
+
+  double ix, iy, ox, oy, tx, ty, lx, rx, ly, ry, i, Offset, stepval, data, angle;
+  double degtorad = .0174532778;
+  static double px = cx, py = cy, pix = cx, piy = cy, plx = cx, ply = cy, prx = cx, pry = cy;
+
+  if (Redraw) {
+    Redraw = false;
+    // draw the dial only one time--this will minimize flicker
+    d.fillRect(0, 0,  127 , 16, SSD1306_WHITE);
+    d.setTextColor(SSD1306_BLACK, SSD1306_WHITE);
+    d.setTextSize(1);
+    d.setCursor(2, 4);
+    d.println(label);
+    // center the scale about the vertical axis--and use this to offset the needle, and scale text
+    Offset = (270 +  sa / 2) * degtorad;
+    // find hte scale step value based on the hival low val and the scale sweep angle
+    // deducting a small value to eliminate round off errors
+    // this val may need to be adjusted
+    stepval = ( inc) * (double (sa) / (double (hival - loval))) + .00;
+    // draw the scale and numbers
+    // note draw this each time to repaint where the needle was
+    for (i = 0; i <= sa; i += stepval) {
+      angle = ( i  * degtorad);
+      angle = Offset - angle ;
+      ox =  (r - 2) * cos(angle) + cx;
+      oy =  (r - 2) * sin(angle) + cy;
+      ix =  (r - 10) * cos(angle) + cx;
+      iy =  (r - 10) * sin(angle) + cy;
+      tx =  (r + 10) * cos(angle) + cx + 8;
+      ty =  (r + 10) * sin(angle) + cy;
+      d.drawLine(ox, oy, ix, iy, SSD1306_WHITE);
+      d.setTextSize(1);
+      d.setTextColor(SSD1306_WHITE, SSD1306_BLACK);
+      d.setCursor(tx - 10, ty );
+      data = hival - ( i * (inc / stepval)) ;
+      d.println(data, dig);
+    }
+    for (i = 0; i <= sa; i ++) {
+      angle = ( i  * degtorad);
+      angle = Offset - angle ;
+      ox =  (r - 2) * cos(angle) + cx;
+      oy =  (r - 2) * sin(angle) + cy;
+      d.drawPixel(ox, oy, SSD1306_WHITE);
+    }
+  }
+  // compute and draw the needle
+  angle = (sa * (1 - (((curval - loval) / (hival - loval)))));
+  angle = angle * degtorad;
+  angle = Offset - angle  ;
+  ix =  (r - 10) * cos(angle) + cx;
+  iy =  (r - 10) * sin(angle) + cy;
+  // draw a triangle for the needle (compute and store 3 vertiticies)
+  lx =  2 * cos(angle - 90 * degtorad) + cx;
+  ly =  2 * sin(angle - 90 * degtorad) + cy;
+  rx =  2 * cos(angle + 90 * degtorad) + cx;
+  ry =  2 * sin(angle + 90 * degtorad) + cy;
+
+  // blank out the old needle
+  d.fillTriangle (pix, piy, plx, ply, prx, pry, SSD1306_BLACK);
+
+  // then draw the new needle
+  d.fillTriangle (ix, iy, lx, ly, rx, ry, SSD1306_WHITE);
+
+  // draw a cute little dial center
+  d.fillCircle(cx, cy, 3, SSD1306_WHITE);
+
+  //save all current to old so the previous dial can be hidden
+  pix = ix;
+  piy = iy;
+  plx = lx;
+  ply = ly;
+  prx = rx;
+  pry = ry;
+  // up until now print sends data to a video buffer NOT the screen
+  // this call sends the data to the screen
+  d.display();
+
+}
